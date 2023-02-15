3e940c3c7784593b91370b545bd8eed3528909a9
wantorres315 <wanessa.araujo.torres@gmail.com>
2023-02-15 08:56:50 +0000

    last changes

---
+++ resources/views/associar.blade.php
@@ -0,0 +1,108 @@
+@extends('layout.internas')
+
+@section('title', getConfig('Associe-se')->nome)
+
+@section('content')
+
+           
+           
+
+           
+            <section class="section-xl bg-periglacial-blue text-center">
+              <div class="shell">
+                <div class="range range-sm-center">
+                  <div class="cell-sm-12 cell-md-12 cell-lg-12">
+             
+                    <!-- RD Mailform-->
+                    <form class="rd-mailform" data-form-output="form-output-global" data-form-type="contact" method="post" action="/associar">
+                    <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
+                      <div class="range range-sm-bottom range-15">
+                        <div class="cell-sm-12">
+                          <div class="form-group">
+                            <label class="form-label-outside" for="contact-first-name">Nome Completo <m style = "color:red">*</m></label>
+                            <input class="form-control" id="nome_completo" required type="text" name="nome" >
+                          </div>
+                        </div>
+                        <div class="cell-sm-4">
+                          <div class="form-group">
+                            <label class="form-label-outside" for="contact-last-name">E-mail <m style = "color:red">*</m></label>
+                            <input class="form-control" id="email" required type="email" name="email" >
+                          </div>
+                        </div>
+                        <div class="cell-sm-4">
+                          <div class="form-group">
+                            <label class="form-label-outside" for="contact-last-name">Telefone <m style = "color:red">*</m></label>
+                            <input class="form-control" id="telefone"required  type="text" name="telefone" >
+                          </div>
+                        </div>
+                        <div class="cell-sm-4">
+                          <div class="form-group">
+                            <label class="form-label-outside" for="contact-last-name">Telemovel <m style = "color:red">*</m></label>
+                            <input class="form-control" id="telemovel" required type="text" name="telemovel" >
+                          </div>
+                        </div>
+                        <div class="cell-sm-6">
+                          <div class="form-group">
+                            <label class="form-label-outside" for="contact-last-name">Nacionalidade <m style = "color:red">*</m></label>
+                            <input class="form-control" id="nacionalidade" required type="text" name="nacionalidade" >
+                          </div>
+                        </div>
+                        <div class="cell-sm-6">
+                          <div class="form-group">
+                            <label class="form-label-outside" for="contact-last-name">Data de Nascimento <m style = "color:red">*</m></label>
+                            <input class="form-control" id="nascimento" required type="date" name="nascimento" >
+                          </div>
+                        </div>
+                        <div class="cell-sm-4">
+                          <div class="form-group">
+                            <label class="form-label-outside" for="contact-last-name">Documento de Identificação <m style = "color:red">*</m></label>
+                            <input  id="contact-email" type="radio" value = "passaporte" checked name="doc_type" >Passaporte
+                            <input  id="contact-email" type="radio" value = "nif" name="doc_type" >NIF
+                            <input  id="contact-email" type="radio" value = "outro" name="doc_type" >Outro
+                          </div>
+                        </div>
+                        <div class="cell-sm-4">
+                          <div class="form-group">
+                            <label class="form-label-outside" for="contact-last-name">Numero do Documento <m style = "color:red">*</m></label>
+                            <input class="form-control" id="numero_documento" required type="text" name="documento" >
+                          </div>
+                        </div>
+                        <div class="cell-sm-4">
+                          <div class="form-group">
+                            <label class="form-label-outside" for="contact-last-name">Código Postal <m style = "color:red">*</m></label>
+                            <input class="form-control" id="codigo_postal" required type="text" name="codigo_postal" >
+                          </div>
+                        </div>
+                        <div class="cell-xs-12">
+                          <div class="form-group">
+                            <label class="form-label-outside" for="contact-message">Endereço Completo (Morada) <m style = "color:red">*</m></label>
+                            <textarea class="form-control" id="endereco" required name="endereco" ></textarea>
+                          </div>
+                        </div>
+                        <div class="cell-xs-12">
+                          <div class="form-group">
+                            <label class="form-label-outside" for="contact-message">Dependentes</label>
+                            <button class="btn btn-sm btn-warning btn-block btn-circle add_dependente" type="button" onclick="cloneNode()">Adicionar Dependente</button>
+                          </div>
+                        </div>
+                        <div id = "dependentes" class = "cell-xs-12">
+
+                        </div>
+                        <div class="cell-sm-12">
+                          <div class="form-group">
+                             <input  type="checkbox" name="acordo" id = "acordo" style = "float: left;margin-right: 10px;">
+                             <label class="form-label-outside" id = "label_acordo" for="contact-email">De acordo com o Regulamento Geral de Proteção de Dados, de 25 de maio de 2018, ao preencher este formulário concordo de maneira livre, informada e inequívoca com o tratamento dos meus dados pessoais somente para o fim de armazenamento, comunicação, inscrição e direcionamento  para as atividades da Associação de Brasileiros do Distrito de Vila Real.</label>
+                          
+                          </div>
+                        </div>
+                        <div class="cell-sm-12 offset-custom-1">
+                          <button class="btn btn-sm btn-primary btn-block btn-circle form_enviar" type="button">Efetuar Cadastro</button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </section>
+           
+@endsection
---
+++ resources/views/modulos/parceiros.blade.php
@@ -0,0 +1,60 @@
+<section class="section-xl bg-gray-dark bg-image text-center" style="background-image: url(images/bg-image-1.jpg);">
+        <div class="shell">
+          <div class="range range-50">
+            <div class="cell-xs-12">
+              @if(empty($show_all))
+                <h2>{{ getConfig('text_parceiros')->valor }}</h2>
+              @endif
+              <div class="p text-width-medium">
+                <p class="big">{{ getConfig('descricao_parceiros')->valor }}</p>
+              </div>
+            </div>
+            <div class="cell-xs-12">
+            <div class="range range-30">
+              @foreach($parceiros as $parceiro)
+              
+                <div class="cell-sm-6 cell-md-4 height-fill">
+                  <div class="thumbnail-card"><img class="thumbnail-card-image" src="/{{ str_replace('public','storage', $parceiro->logo)}}" alt="" width="370" height="310"/>
+                    <div class="thumbnail-card-body">
+                      <p class="thumbnail-card-header" data-toggle="modal" data-target="#modalWindow">{{$parceiro->nome}}</p>
+                      <div class="thumbnail-card-text">
+                        <p>{{$parceiro->observacoes}}</p>
+                        
+                        <p>
+                          @if($parceiro->telefone || $parceiro->telemovel) <i class = "fa fa-phone"></i> @endif 
+                          @if($parceiro->telefone)<a href = "tel:{{$parceiro->telefone}}">{{$parceiro->telefone}}</a>@endif 
+                          @if($parceiro->telefone && $parceiro->telemovel) / @endif 
+                          @if($parceiro->telemovel) <a href = "tel:{{$parceiro->telemovel}}"> {{$parceiro->telemovel}} @endif </a></p>
+                         
+                      </div>
+                      <ul class="inline-list inline-list-md">
+                        @if($parceiro->facebook)
+                          <li><a class="icon icon-xs link-gray-base fa-facebook" href="{{$parceiro->facebook}}"></a></li>
+                        @endif
+                        @if($parceiro->instagram)
+                          <li><a class="icon icon-xs link-gray-base fa-instagram" href="{{$parceiro->instagram}}"></a></li>
+                        @endif
+                        @if($parceiro->site)
+                          <li><a class="icon icon-xs link-gray-base fa-link" href="{{$parceiro->site}}"></a></li>
+                        @endif
+                        @if($parceiro->email)
+                          <li><a class="icon icon-xs link-gray-base fa-envelope"href = "mailto:{{$parceiro->email}}"></a></li>
+                        @endif
+                        
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                
+              
+              @endforeach
+              </div>
+            </div>
+            @if(empty($show_all))
+            <div class="cell-xs-12"><a class="btn btn-sm btn-style-1 btn-primary" href="/parceiros">{{ getConfig('botao_ver_parceiros')->valor }}</a></div>
+            @endif
+          </div>
+        </div>
+      </section>
+
+ 
